0e0dec317568176df94d3443dd1e7b5b0ea3cfd7
jinzlgit <1436315008@qq.com>
2021-07-14 09:12:16 +0800

    分支优化导包

--- src/main/java/com/jinzl/netty/basic/NettyDiscardServer.java
+++ src/main/java/com/jinzl/netty/basic/NettyDiscardServer.java
@@ -8,8 +8,6 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * 丢弃服务器
